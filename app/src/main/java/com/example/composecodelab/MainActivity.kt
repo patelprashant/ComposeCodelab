d14d75db189efae7b70fef765a83024baf5f0947
Prashant <prashant.patel77@gmail.com>
2024-06-23 21:44:32 +1000

    Added show more button

--- app/src/main/java/com/example/composecodelab/MainActivity.kt
+++ app/src/main/java/com/example/composecodelab/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -48,9 +50,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(modifier = modifier.fillMaxWidth().padding(24.dp)) {
-            Text(text = "Hello")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = modifier.weight(1f)) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { /* Do something! */ }) {
+                Text("Show More")
+            }
         }
     }
 }
